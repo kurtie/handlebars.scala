d37835da49174c5e119b9068fb0e07d9f5eeab15
kurt <kurt@zentense.com>
2016-08-05 15:55:44 +0200

    Added first and last to helper 'each'

--- src/main/scala/com/gilt/handlebars/scala/helper/EachHelper.scala
+++ src/main/scala/com/gilt/handlebars/scala/helper/EachHelper.scala
@@ -5,21 +5,30 @@ import com.gilt.handlebars.scala.binding.{Binding, BindingFactory}
 class EachHelper[T] extends Helper[T] {
   def apply(binding: Binding[T], options: HelperOptions[T])(implicit contextFactory: BindingFactory[T]): String = {
     val arg0 = options.argument(0)
-    if (arg0.isDictionary)
-      arg0.asDictionaryCollection.zipWithIndex.map {
+    if (arg0.isDictionary) {
+      val col= arg0.asDictionaryCollection
+      col.zipWithIndex.map {
         case ((key, value), idx) =>
           options.visit(value,
             Map(
+	      "first" -> contextFactory.bindPrimitive(idx==0),
               "key" -> contextFactory.bindPrimitive(key),
-              "index" -> contextFactory.bindPrimitive(idx)))
+              "index" -> contextFactory.bindPrimitive(idx),
+	      "last" -> contextFactory.bindPrimitive( idx != col.size )
+           ))
       }.mkString
-    else if (arg0.isCollection)
-      arg0.asCollection.zipWithIndex.map {
+    } else if (arg0.isCollection) {
+      val col= arg0.asCollection
+      col.zipWithIndex.map {
         case (value, idx) =>
           options.visit(value,
             Map(
-              "index" -> contextFactory.bindPrimitive(idx)))
+	      "first" -> contextFactory.bindPrimitive(idx==0),
+              "index" -> contextFactory.bindPrimitive(idx),
+	      "last" -> contextFactory.bindPrimitive( idx == col.size-1 )
+           ))
       }.mkString
+    }
 
     else {
 //      warn("Could not iterate over argument for {{#each}}")
